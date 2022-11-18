b21da5528ba56151af37e997eae78c248e633245
Gabriel Alves dos Santos <gabrielalvesdossantos16@gmail.com>
2022-11-18 09:30:12 -0300

    Modificação para suporte a rotas react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Home from './pages/home';
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Teste from './pages/teste';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+      <Route path='/' element={<Home />} />
+        <Route path='/teste' element={<Teste />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
