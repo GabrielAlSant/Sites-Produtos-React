b6e6164152e47f11612778341dfe23376f755222
Gabriel Alves dos Santos <gabrielalvesdossantos16@gmail.com>
2022-11-18 11:32:29 -0300

    Produtos em card

--- src/components/cardProdutos.jsx
+++ src/components/cardProdutos.jsx
@@ -1,14 +1,18 @@
 import React from "react";
 import Produtos from "../pages/produtos";
+import "../index.css"
 
 export default function CardProdutos(props){
-    return(
-        <div class="card-body">
-        <div>
-            <span class="autor">{props.titulo} </span>
-            <span class="autor">{props.preco}</span>
-        </div>
-         <h5>{props.descricao}</h5>
-    </div>
+    return(   
+        <div class="card tamanho">
+  <img src={props.img} class="card-img-top" alt="..." />
+  <div class="card-body">
+    <h5 class="card-title">{props.titulo}</h5>
+    <p class="card-text">{props.descricao}</p>
+    <p class="card-text">preço:{props.preco}</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+</div>
     )
 }
+
