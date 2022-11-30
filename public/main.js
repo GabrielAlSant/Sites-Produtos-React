b6b65b09dd0b9c6ceead8cc4504d4cc17e2f140e
gabrielalsant <gabrielalvesdossantos16@gmail.com>
2022-11-30 09:53:50 -0300

    pwa

--- public/main.js
+++ public/main.js
@@ -2,7 +2,7 @@ window.addEventListener('load', e => {
     postNews();
     "use strict";//restrito a funcionar em navegadores comES6 >
     if("serviceWorker" in navigator){
-        navigator.serviceWorker.register("./sw.js");
+        navigator.serviceWorker.register("./worker.js");
     }
 }); 
 
--- src/index.js
+++ src/index.js
@@ -14,5 +14,4 @@ root.render(
   </React.StrictMode>
   </AuthContextProvider>
 );
-
 reportWebVitals();
